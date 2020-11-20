77bd2873882ac4fe6b53fdece6e2f1bce43d4404
christinapassafaro <christinapassafaro@gmail.com>
2020-11-20 11:16:24 -0500

    first bonus

--- lib/script.py
+++ lib/script.py
@@ -1,17 +1,60 @@
 from capitals import states
 import random 
 
+# random.shuffle(states)
+# # print(states)
+# #game being called
+# def game_intro():
+#     print('Welcome to States and Capitals! The game were we give you the state and you give us the capital')
+#     user = input('Ready to play? y or n:')
+#     if(user == 'y'):
+#         play_game()
+#     else:
+#         print('exit')
+
+
+
+# def play_game():
+#     incorrect_score = 0
+#     correct_score = 0
+#     #test states below!!!!!!!!!!!!!!!!!!!!!!!!!!
+#     # states1 = [{'name': 'Virginia', 'capital': 'Richmond'}, {'name': 'Washington', 'capital': 'Olympia'}, {'name': 'West Virginia', 'capital': 'Charleston'}, {'name': 'Wisconsin', 'capital': 'Madison'}, {'name': 'Wyoming', 'capital': 'Cheyenne'}]
+#     for i in range(len(states)):
+#         # print(state[i])
+#         state = states[i]['name']
+#         capital = states[i]['capital']
+    
+#         answer = input(f'What is the capital of {state}?:')
+#         if(answer == capital):
+#             print(f'Correct, the answer is {capital}')
+#             correct_score = correct_score + 1
+#             print(f'Correct: {correct_score} Incorrect:{incorrect_score}')
+#         else:
+#             print(f'Incorrect, the answer is {capital}')
+#             incorrect_score = incorrect_score + 1
+#             print(f'Correct: {correct_score} Incorrect:{incorrect_score}')
+#     final_score = correct_score - incorrect_score
+#     print(f'Total Score:{final_score}')
+#     print(f'Correct: {correct_score} Incorrect:{incorrect_score}')
+#     play_again = input('Would you like to play again? y or n:')
+#     if(play_again == 'y'):
+#         game_intro()
+#     else:
+#         print('Thanks for playing!')
+
+
+# game_intro()
+
+###bonuses#####################################
+
+
 random.shuffle(states)
 # print(states)
-
-
-#first functino should introduce game and ask for player input
-#on if they want to play, if yes call playgame function if not exit
+#game being called
 def game_intro():
     print('Welcome to States and Capitals! The game were we give you the state and you give us the capital')
     user = input('Ready to play? y or n:')
     if(user == 'y'):
-        #call playergame function
         play_game()
     else:
         print('exit')
@@ -21,10 +64,11 @@ def game_intro():
 def play_game():
     incorrect_score = 0
     correct_score = 0
-    #test states below!!!!!!!!!!!!!!!!!!!!!!!!!!
+    print(states)
+    #test states below!
     # states1 = [{'name': 'Virginia', 'capital': 'Richmond'}, {'name': 'Washington', 'capital': 'Olympia'}, {'name': 'West Virginia', 'capital': 'Charleston'}, {'name': 'Wisconsin', 'capital': 'Madison'}, {'name': 'Wyoming', 'capital': 'Cheyenne'}]
     for i in range(len(states)):
-        # print(state[i])
+        # print(states1)
         state = states[i]['name']
         capital = states[i]['capital']
     
@@ -33,10 +77,24 @@ def play_game():
             print(f'Correct, the answer is {capital}')
             correct_score = correct_score + 1
             print(f'Correct: {correct_score} Incorrect:{incorrect_score}')
+            #trying to make new key
+            if 'Correct' in states[i].keys():   
+                states[i]['Correct'] += 1
+                print(states[i])
+            else:
+                states[i]['Correct'] = states[i].get('Correct', 0) +1
+                print(states[i])
         else:
             print(f'Incorrect, the answer is {capital}')
             incorrect_score = incorrect_score + 1
             print(f'Correct: {correct_score} Incorrect:{incorrect_score}')
+            if 'Incorrect' in states[i].keys():   
+                states[i]['Incorrect'] += 1
+                print(states[i])
+            else:
+                states[i]['Incorrect'] = states[i].get('Incorrect', 0) +1
+                print(states[i])
+
     final_score = correct_score - incorrect_score
     print(f'Total Score:{final_score}')
     print(f'Correct: {correct_score} Incorrect:{incorrect_score}')
@@ -45,6 +103,11 @@ def play_game():
         game_intro()
     else:
         print('Thanks for playing!')
+    return states
 
+# def key_incrementor(state):
+#     print(state)
+#     # state['Correct'] 
 
+states = play_game()
 game_intro()
